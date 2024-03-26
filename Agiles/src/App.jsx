86e56891a8d8e8987a56307eda5cbb342cd9c513
GinoGallina <ginogallina2002@gmail.com>
2024-03-25 21:16:03 -0300

    calculadora

--- Agiles/src/App.jsx
+++ Agiles/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react';
+import './App.css'; // Importa los estilos CSS
 
-function App() {
-  const [count, setCount] = useState(0)
+const Calculator = () => {
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setResult('');
+  };
+
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result) || '');
+    } catch (error) {
+      setResult('Error');
+    }
+  };
 
   return (
-    <>
+    <div className="calculator">
+      <input type="text" value={result} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="clear" onClick={clear}>Clear</button>
+        <button onClick={backspace}>C</button>
+        <button name="/" className="operator" onClick={handleClick}>/</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="*" className="operator" onClick={handleClick}>*</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="-" className="operator" onClick={handleClick}>-</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="+" className="operator" onClick={handleClick}>+</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button className="operator" onClick={calculate}>=</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default Calculator;
